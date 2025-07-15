014cfab74de0f3551f2cfc1e979e6faccb386b4e
Camila Obando <camila12obando@gmail.com>
2025-07-15 13:04:36 -0500

    feat: update main branch with latest changes

--- src/components/career-selector.js
+++ src/components/career-selector.js
@@ -12,31 +12,31 @@ this.careers = [
     nombre: 'INTELIGENCIA ARTIFICIAL',
     facultad: 'DISEÑO Y ADM DE BASE DE DATOS',
     descripcion: 'Curso avanzado sobre modelos inteligentes aplicados.',
-    imagen: 'https://via.placeholder.com/300x150?text=IA'
+    imagen: 'https://tse3.mm.bing.net/th/id/OIP.Q46xMzaA-zjDMNGu_nbCDQHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3'
   },
   {
     nombre: 'MODELADO AVAN DE BASE DE DATOS',
     facultad: 'DISEÑO Y ADM DE BASE DE DATOS',
     descripcion: 'Enseña técnicas de modelado relacional y orientado a objetos.',
-    imagen: 'https://via.placeholder.com/300x150?text=BD+Avanzado'
+    imagen: 'https://i.ytimg.com/vi/K0pLUQLRLzs/maxresdefault.jpg'
   },
   {
     nombre: 'DESARROLLO WEB PARA INTEGRACIO',
     facultad: 'DESA ANALI SOFTWARE Y APLICACI',
     descripcion: 'Desarrollo de aplicaciones web modernas con frameworks.',
-    imagen: 'https://via.placeholder.com/300x150?text=Web+Integracion'
+    imagen: 'https://www.quois.com/wp-content/uploads/2021/07/Las-fases-del-desarrollo-de-una-web.jpg'
   },
   {
     nombre: 'LECT. ESC. TEXTOS ACADEMICOS',
     facultad: 'PROG INTERDISCIPLINARIOS TICS',
     descripcion: 'Técnicas de lectura y escritura académica.',
-    imagen: 'https://via.placeholder.com/300x150?text=Textos+Academicos'
+    imagen: 'https://tse2.mm.bing.net/th/id/OIP.G7AybaljmhWOMdLet_c86wHaE8?r=0&rs=1&pid=ImgDetMain&o=7&rm=3'
   },
   {
     nombre: 'PROGRAM INTEGRATIVA DE COMPONE',
     facultad: 'DESA ANALI SOFTWARE Y APLICACI',
     descripcion: 'Proyecto integrador usando componentes web con Lit.',
-    imagen: 'https://via.placeholder.com/300x150?text=Integrativa+Componentes'
+    imagen: 'https://tse1.explicit.bing.net/th/id/OIP.NZjVu6PfbE6OOLUQ0KLwTAHaEo?r=0&rs=1&pid=ImgDetMain&o=7&rm=3'
   }
 ];
 
